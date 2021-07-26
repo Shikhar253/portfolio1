66850407c41cf88a6c3c4b2853ae93dbbd9dd831
Shikhar253 <saxenashikhar01@gmail.com>
2021-07-26 18:12:03 +0530

    .

--- index.js
+++ index.js
@@ -54,21 +54,6 @@ navClose.addEventListener("click", () => {
   }
 });
 
-$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-          $('html, body').animate({
-              scrollTop: (target.offset().top - navHeight + 5)
-          }, 1000, "easeInOutExpo");
-          return false;
-      }
-  }
-});
-$('.js-scroll').on("click", function() {
-  $('.navbar-collapse').collapse('hide');
-});
 
 // Colors
 
