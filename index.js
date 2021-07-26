1426daef5eba66c305b14134353be3f42fe2a997
Shikhar253 <saxenashikhar01@gmail.com>
2021-07-27 03:54:45 +0530

    .

--- index.js
+++ index.js
@@ -82,27 +82,27 @@ window.addEventListener("scroll", () => {
 
 // Glidejs
 
-const glide = document.querySelector(".glide");
+// const glide = document.querySelector(".glide");
 
-if (glide)
-  new Glide(glide, {
-    type: "carousel",
-    startAt: 0,
-    perView: 3,
-    gap: 30,
-    hoverpause: true,
-    autoplay: 2000,
-    animationDuration: 800,
-    animationTimingFunc: "linear",
-    breakpoints: {
-      996: {
-        perView: 2,
-      },
-      768: {
-        perView: 1,
-      },
-    },
-  }).mount();
+// if (glide)
+//   new Glide(glide, {
+//     type: "carousel",
+//     startAt: 0,
+//     perView: 3,
+//     gap: 30,
+//     hoverpause: true,
+//     autoplay: 2000,
+//     animationDuration: 800,
+//     animationTimingFunc: "linear",
+//     breakpoints: {
+//       996: {
+//         perView: 2,
+//       },
+//       768: {
+//         perView: 1,
+//       },
+//     },
+//   }).mount();
 
 AOS.init();
 
